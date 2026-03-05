68600ca7a17936a34a240e5affae849ab424b682
Ali Mohamed <76254631+alikashlan10@users.noreply.github.com>
2026-03-05 02:51:06 +0200

    ngrok api url

--- src/api/searchAPI.js
+++ src/api/searchAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const searchHadiths = async (query, top_k = 5) => {
-  const response = await axios.post(`${API_URL}/search`, {
+  const response = await axios.post(`${"https://3a6f-197-45-204-181.ngrok-free.app"}/search`, {
     query,
     top_k,
   });
