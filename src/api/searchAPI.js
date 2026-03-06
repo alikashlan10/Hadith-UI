9725782a6bb63813b30d18caf8a80fd6d88ebdad
Ali Mohamed <76254631+alikashlan10@users.noreply.github.com>
2026-03-06 18:44:50 +0200

    add top_k to dependencies

--- src/api/searchAPI.js
+++ src/api/searchAPI.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
+const TOP_K = import.meta.env.TOP_K
 
-export const searchHadiths = async (query, top_k = 5) => {
+export const searchHadiths = async (query, top_k = TOP_K) => {
   const response = await axios.post(`${API_URL}/search`, {
     query,
     top_k,
