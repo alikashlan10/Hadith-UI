b6e13e145d700acb2578fa5535fe292dc7dbab21
Ali Mohamed <76254631+alikashlan10@users.noreply.github.com>
2026-03-06 18:50:33 +0200

    MINOR FIX

--- src/api/searchAPI.js
+++ src/api/searchAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
-const TOP_K = import.meta.env.VITE_TOP_K
+const TOP_K = import.meta.env.VITE_TOP_K;
 
 export const searchHadiths = async (query, top_k = TOP_K) => {
   const response = await axios.post(`${API_URL}/search`, {
